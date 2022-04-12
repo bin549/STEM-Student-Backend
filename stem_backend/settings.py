a601c5118d75368842c21f03693fb7dd4dfdf9a0
bin549 <549288160@qq.com>
2022-04-12 21:22:09 +0800

    3

--- stem_backend/settings.py
+++ stem_backend/settings.py
@@ -87,7 +87,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+ 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
